1fe6d9e6d76320748d36b3830aab958f85b65a04
Juan C.Parra <jcamiloparra7.0@gmail.com>
2021-12-23 22:00:00 -0500

    commit classes_stage

--- Ch2 - Basics/classes_start.py
+++ Ch2 - Basics/classes_start.py
@@ -3,3 +3,29 @@
 # LinkedIn Learning Python course by Joe Marini
 #
 
+class Vehicle():
+    def __init__(self, bodystyle):
+        self.bodystyle = bodystyle
+
+        if self.bodystyle == 'Car':
+            self.wheels = 4
+        elif self.bodystyle == 'Motorcycle':
+            self.wheels = 2
+
+
+class Car(Vehicle):
+    def __init__(self, enginetype):
+        super().__init__('Car')
+        self.enginetype = enginetype
+
+class Motorcycle(Vehicle):
+    def __init__(self, enginetype):
+        super().__init__('Motorcycle')
+        self.enginetype = enginetype
+
+moto1 = Motorcycle('Gasoline')
+
+print(moto1.wheels)
+print(moto1.enginetype)
+print(moto1.bodystyle)
+
