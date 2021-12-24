91be297f8bdc608e59f5258424f3fbcbbf801b41
Juan C.Parra <jcamiloparra7.0@gmail.com>
2021-12-23 22:27:04 -0500

    commit

--- Ch2 - Basics/import_classes.py
+++ Ch2 - Basics/import_classes.py
@@ -1,5 +1,9 @@
-from classes_start import Vehicle, Car, Motorcycle
+from classes_start import Car, Motorcycle, Vehicle
 
 car1 = Car('Electric')
+car1.drive(35)
 
-print(Car.enginetype)
+print(car1.enginetype)
+print(car1.wheels)
+print(car1.speed)
+print(car1.mode)
