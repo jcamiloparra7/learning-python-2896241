99c469b348414578fc957a0e295fb98ad6b41f06
Juan C.Parra <jcamiloparra7.0@gmail.com>
2021-12-23 22:10:59 -0500

    commit

--- Ch2 - Basics/classes_start.py
+++ Ch2 - Basics/classes_start.py
@@ -12,11 +12,23 @@ class Vehicle():
         elif self.bodystyle == 'Motorcycle':
             self.wheels = 2
 
+    def drive(self, speed):
+        self.speed = speed
+        if self.speed > 0:
+            self.mode = 'Forward'
+        elif self.speed == 0:
+            self.mode = 'Station'
+        else:
+            self.mode = 'Reverse' 
+        
+
 
 class Car(Vehicle):
     def __init__(self, enginetype):
         super().__init__('Car')
         self.enginetype = enginetype
+
+
 
 class Motorcycle(Vehicle):
     def __init__(self, enginetype):
@@ -24,8 +36,11 @@ class Motorcycle(Vehicle):
         self.enginetype = enginetype
 
 moto1 = Motorcycle('Gasoline')
+moto1.drive(-1)
 
 print(moto1.wheels)
 print(moto1.enginetype)
 print(moto1.bodystyle)
+print(moto1.speed)
+print(moto1.mode)
 
